2ebd915cf8ad0f08e445261bdfabdff964858e9c
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2020-07-08 22:39:39 +0200

    Test file context

--- FamilyTasks/FamilyTasks/Server/Startup.cs
+++ FamilyTasks/FamilyTasks/Server/Startup.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Linq;
 using FileContextCore;
 using FamilyTasks.Infrastructure.Contexts;
 
@@ -26,7 +23,8 @@ namespace FamilyTasks.Server
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
-            services.AddDbContext<ApplicationDbContext>(options => options.UseFileContextDatabase(location: Configuration.GetValue<string>("FilePath")));
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseFileContextDatabase(location: Configuration.GetValue<string>("FilePath")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
