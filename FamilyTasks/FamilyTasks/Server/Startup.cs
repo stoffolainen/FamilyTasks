c5f69cd7be0290a5ff0ed4c2f2a6039109f86bf1
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2020-07-09 00:15:50 +0200

    Add identity

--- FamilyTasks/FamilyTasks/Server/Startup.cs
+++ FamilyTasks/FamilyTasks/Server/Startup.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using FamilyTasks.Domain.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -5,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using FileContextCore;
 using FamilyTasks.Infrastructure.Contexts;
+using Microsoft.AspNetCore.Identity;
 
 namespace FamilyTasks.Server
 {
@@ -21,10 +24,32 @@ namespace FamilyTasks.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            services.AddRazorPages();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseFileContextDatabase(location: Configuration.GetValue<string>("FilePath")));
+
+            services.AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = false;
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,14 +70,15 @@ namespace FamilyTasks.Server
             app.UseHttpsRedirection()
                 .UseBlazorFrameworkFiles()
                 .UseStaticFiles()
-                .UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-                endpoints.MapFallbackToFile("index.html");
-            });
+                .UseRouting()
+                .UseAuthentication()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                    endpoints.MapControllers();
+                    endpoints.MapFallbackToFile("index.html");
+                })
+                .UseAuthorization();
         }
     }
 }
